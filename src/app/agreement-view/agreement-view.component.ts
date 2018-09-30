4ae5ab33836235b355e61ce8c19bfc3ea89c1032
kolejarzyk <pawel.dee@live.com>
2018-09-30 14:25:31 +0200

    agreement view component

---
+++ src/app/agreement-view/agreement-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agreement-view',
+  templateUrl: './agreement-view.component.html',
+  styleUrls: ['./agreement-view.component.css']
+})
+export class AgreementViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from  '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
 
 import { AppComponent } from './app.component';
+import { AgreementViewComponent } from './agreement-view/agreement-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgreementViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
