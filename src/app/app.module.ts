003b2c20d57fd867621a635e154608314141b8b6
Guziec-Lobuziec <oliversliwonik@o2.pl>
2018-10-04 18:02:11 +0200

    IS3 moving AgreementView to separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from  '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { PanelModule } from 'primeng/panel';
-import { TableModule} from 'primeng/table';
 import { AppComponent } from './app.component';
 import { AgreementViewModule } from './agreement-view/agreement-view.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AgreementViewModule
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    PanelModule,
-    TableModule
+    AgreementViewModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
