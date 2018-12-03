6ff7443ad8b6f4d7d2b935716ed50fdb79e630e3
hentaininja <hasamt94@gmail.com>
2018-12-03 19:53:18 +0100

    routing tests

--- src/app/agreement-view/agreement-view-routing.module.ts
+++ src/app/agreement-view/agreement-view-routing.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {AgreementViewComponent} from "./agreement-view/agreement-view.component";
+import {toCheckSumAddress} from "./share/model/routing";
 
 
 const routes: Routes = [
 	{
-		path: "agreement",
+		path: "agreement/" + toCheckSumAddress(),
 		component: AgreementViewComponent
   }
 ];
