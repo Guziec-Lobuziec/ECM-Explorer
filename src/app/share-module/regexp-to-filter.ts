c5a626aa4b8b2ad5f7bd991a4c68a273cb9f2c56
Kolejarzyk <pawel.dee@live.com>
2018-12-03 19:58:57 +0100

    price reg factory

--- src/app/share-module/regexp-to-filter.ts
+++ src/app/share-module/regexp-to-filter.ts
@@ -1,6 +1,7 @@
 import { IFilterFactory } from "./ifilter-factory";
 import { IQuery } from "./iquery";
 import { NameFilter } from "./name-filter";
+import { PriceFilter } from "./price-filter";
 
 export const regToFactory:[RegExp, IFilterFactory,  (m:string[]) => string][] = [
   [
@@ -8,6 +9,15 @@ export const regToFactory:[RegExp, IFilterFactory,  (m:string[]) => string][] = 
     {
       create: (value:string, inner?:IQuery) => {
         return new NameFilter(value, inner);
+      }
+    },
+    (m:string[]) => m[1]
+  ],
+  [
+    new RegExp('price:((\s*[^:\d]*)*)(?!(\w*:))'),
+    {
+      create: (value: number, inner?:IQuery) => {
+        return new PriceFilter(value,inner);
       }
     },
     (m:string[]) => m[1]
