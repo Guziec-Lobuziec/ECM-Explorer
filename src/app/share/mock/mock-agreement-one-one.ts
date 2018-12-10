b86114af396668a287711352f439636acf73f240
Guziec-Lobuziec <oliversliwonik@o2.pl>
2018-12-10 10:30:50 +0100

    after merge fix

--- src/app/share/mock/mock-agreement-one-one.ts
+++
@@ -1,9 +0,0 @@
-import { AgreementOneOne } from "../model/agreement-one-one";
-
-export const MockAgreementOneOne: AgreementOneOne = 
-{
-    name: "TestAgreement",
-    price: 0.0000000023,
-    description: "This is test agreement. Nice to have you.",
-    blocksToExpire: 20673
-}
--- src/app/share/model/routing-tests.ts
+++
@@ -1,8 +0,0 @@
-import {Routing} from "./routing";
-
-describe('RoutingValidationTests', () => {
-    
-    it('address should match', () => {
-        Routing.toChecksumAddress('0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359').toEqual('0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359',"addresses should match")
-    })
-})
