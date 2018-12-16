ca1a50271fe0d59f102b62cce4dd8ddb6263f0d7
Kolejarzyk <pawel.dee@live.com>
2018-12-16 19:48:05 +0100

    fixes to search view

--- src/app/search-view/search-view.module.ts
+++ src/app/search-view/search-view.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { SearchViewComponent } from './search-view/search-view.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SearchViewRoutingModule } from './search-view-routing.module';
+import {DataViewModule} from 'primeng/dataview';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     SearchViewRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DataViewModule,
+    TableModule
   ],
   exports: [SearchViewComponent],
   declarations: [SearchViewComponent]
